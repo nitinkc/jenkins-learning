f5c32cebb4be5919d49fcbf8fc8ef02d11a3e411
Nitin Chaurasia <gs.nitin@gmail.com>
2021-07-15 01:34:18 -0500

    controller added

---
+++ src/main/java/com/learn/devops/TestController.java
@@ -0,0 +1,17 @@
+package com.learn.devops;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String health(){
+        String url = "http://localhost:9000/profile";
+        return "Healthy and running" +"\t"+ url;
+    }
+
+
+}
